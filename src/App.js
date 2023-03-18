c4508970e122425404468419e89d7a1c6cce1a8d
GeorgeFlood <mrgeorgeflood@gmail.com>
2023-03-18 00:47:52 +0000

    search bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
+import SearchBar from "./components/SearchBar";
 import "./styles/main.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <SearchBar />
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,48 +1,75 @@
 import React from "react";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isOpen, SetisOpen] = useState(false);
+  const [font, SetFont] = useState("Serif");
+
+  const handleSvgClick = () => {
+    SetisOpen(!isOpen);
+  };
+
+  const handleFontClick = (e) => {
+    SetFont(e.target.innerHTML);
+  };
+
   return (
-    <div>
-      <div className="container">
-        <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="34"
-            height="38"
-            viewBox="0 0 34 38"
+    <div className="container">
+      <div>
+        <svg
+          className="logo"
+          xmlns="http://www.w3.org/2000/svg"
+          width="34"
+          height="38"
+          viewBox="0 0 34 38"
+        >
+          <g
+            fill="none"
+            fill-rule="evenodd"
+            stroke="#838383"
+            stroke-linecap="round"
+            stroke-width="1.5"
           >
-            <g
-              fill="none"
-              fill-rule="evenodd"
-              stroke="#838383"
-              stroke-linecap="round"
-              stroke-width="1.5"
-            >
-              <path d="M1 33V5a4 4 0 0 1 4-4h26.8A1.2 1.2 0 0 1 33 2.2v26.228M5 29h28M5 37h28" />
-              <path stroke-linejoin="round" d="M5 37a4 4 0 1 1 0-8" />
-              <path d="M11 9h12" />
-            </g>
-          </svg>
-        </div>
+            <path d="M1 33V5a4 4 0 0 1 4-4h26.8A1.2 1.2 0 0 1 33 2.2v26.228M5 29h28M5 37h28" />
+            <path stroke-linejoin="round" d="M5 37a4 4 0 1 1 0-8" />
+            <path d="M11 9h12" />
+          </g>
+        </svg>
+      </div>
+      <div className="options">
         <div className="font-style">
-          <p>Serif</p>
+          <p>{font}</p>
           <svg
             className="font-style__icon"
             xmlns="http://www.w3.org/2000/svg"
             width="14"
             height="8"
             viewBox="0 0 14 8"
+            onClick={handleSvgClick}
           >
             <path
               fill="none"
               stroke="#A445ED"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               d="m1 1 6 6 6-6"
             />
           </svg>
         </div>
+        {isOpen && (
+          <div className={isOpen ? "dropdown-menu" : "no-menu"}>
+            <ul>
+              <li onClick={handleFontClick}>Serif</li>
+              <li onClick={handleFontClick}>Inconsolata</li>
+              <li onClick={handleFontClick}>inter</li>
+              <li onClick={handleFontClick}>Lora</li>
+            </ul>
+          </div>
+        )}
         <div className="night-mode">
-          <input type="checkbox" id="switch" name="switch" />
+          <label>
+            <input type="checkbox" />
+            <span class="slider round"></span>
+          </label>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="22"
@@ -52,9 +79,9 @@ const Navbar = () => {
             <path
               fill="none"
               stroke="#838383"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="1.5"
               d="M1 10.449a10.544 10.544 0 0 0 19.993 4.686C11.544 15.135 6.858 10.448 6.858 1A10.545 10.545 0 0 0 1 10.449Z"
             />
           </svg>
