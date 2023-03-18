1566fcb2118f59c52f4243ea2e9a9cbafb55d724
GeorgeFlood <mrgeorgeflood@gmail.com>
2023-03-18 13:37:57 +0000

    search bar update

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,7 +4,25 @@ const SearchBar = () => {
   return (
     <div>
       <form>
-        <input className="search" type="text" />
+        <div className="search-container">
+          <input className="search" type="text" placeholder="Search..." />
+          <svg
+            className="search-icon"
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            viewBox="0 0 18 18"
+          >
+            <path
+              fill="none"
+              stroke="#A445ED"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="1.5"
+              d="m12.663 12.663 3.887 3.887M1 7.664a6.665 6.665 0 1 0 13.33 0 6.665 6.665 0 0 0-13.33 0Z"
+            />
+          </svg>
+        </div>
       </form>
     </div>
   );
